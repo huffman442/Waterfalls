829a5e0f6caa2b099d048e077748b07db49eb19d
Michael Huffman <huffman442@yahoo.com>
2019-01-03 16:29:45 -0500

    moved movement checks to waterfalls_logic file

--- waterfalls.rb
+++ waterfalls.rb
@@ -4,10 +4,8 @@ require "./waterfalls_data.rb"
 require "./waterfalls_logic.rb"
 input = ""
 game = Game.new
-row_position = 0
-col_position = 0
 gridSize = 20
-#%x ( say 'today you wont be sticking to the rivers and the lakes that youre used to' )
+
 #loading_message = "Today you wont be sticking to the rivers & lakes that you're used to..."
 #loading_message.each_char do |i|
 #     print i
@@ -16,84 +14,16 @@ gridSize = 20
 system "cls"
 puts ""
 while input != "q" 
-    game.display(row_position, col_position)
+    game.display()
     puts $message
-    $counter += 1
-    game.shark_movement()       
+    
+    game.shark_movement(input)       
     
     # get single character input
     input = STDIN.getch
     # character doesn't get typed when a person types it
    
-    if row_position > 0 && $grid[row_position - 1][col_position] != $Island
-        if input == 'w' 
-            if row_position - 1 == $shark_x && col_position == $shark_y
-                print "You hit the shark & you are dead!"
-                input = "q"  
-                
-            end
-            if $grid[row_position - 1][col_position] == $Waterfall
-                message = "You found a waterfall!!!"  
-                $grid[row_position - 1][col_position] = $Empty 
-            
-            else
-                $message = "Water is all around!"     
-            end     
-            row_position = row_position - 1
-                  
-        end
-    end
-    if col_position > 0 && $grid[row_position][col_position - 1] != $Island
-        if input == 'a'
-            if row_position == $shark_x && col_position - 1 == $shark_y
-                print "You hit the shark & you are dead!"
-                sleep(3)
-                input = "q"        
-            end
-            if $grid[row_position][col_position - 1] == $Waterfall
-                message = "You found a waterfall!!!" 
-                #sleep(3) 
-            else
-                message = "Water is all around!"     
-            end
-            col_position = col_position - 1
-            $grid[row_position][col_position] = $Empty
-        end
-    end
-    if row_position < gridSize - 1 && $grid[row_position + 1][col_position] != $Island
-        if input == 's'
-            if row_position + 1 == $shark_x && col_position == $shark_y
-                print "You hit the shark & you are dead!"
-                sleep(3)
-                input = "q"               
-            end
-            if $grid[row_position + 1][col_position] == $Waterfall
-                message = "You found a waterfall!!!"  
-                #sleep(3) 
-            else
-                message = "Water is all around!"     
-            end
-            row_position = row_position + 1
-            $grid[row_position][col_position] = $Empty
-        end
-    end
-    if col_position < gridSize - 1 && $grid[row_position][col_position + 1] != $Island
-        if input == 'd'
-            if row_position == $shark_x && col_position + 1 == $shark_y
-                print "You hit the shark & you are dead!"
-                sleep(3)
-                input = "q"          
-            end
-            if $grid[row_position][col_position + 1] == $Waterfall
-                message = "You found a waterfall!!!"  
-                #sleep(3)    
-            else
-                $message = "Water is all around!" 
-            end
-            col_position = col_position + 1
-            $grid[row_position][col_position] = $Empty
-        end
-    end
+    game.movement(input)
     still_searching = false
     (0...gridSize).each do |i|
         (0...gridSize).each do |j|
--- waterfalls_logic.rb
+++ waterfalls_logic.rb
@@ -10,13 +10,15 @@ class Game
         $counter = 0
         $shark_x = 5
         $shark_y = 3
+        @row_position = 0
+        @col_position = 0
         @gridSize = 20
         @board = Board.new(@gridSize)
     end
-    def display(row_position, col_position)
+    def display()
         (0...@gridSize).each do |i|
             (0...@gridSize).each do |j|
-                if row_position == i && col_position == j
+                if @row_position == i && @col_position == j
                     print "Ω "
                 elsif $shark_x == i && $shark_y == j
                     print "┴ "
@@ -35,21 +37,95 @@ class Game
             puts #print one array, hits enter, prints next array
         end
     end
-    def shark_movement
-        if $counter > 20
-            $counter = 0
+    def shark_movement(input)
+        while $input != q
+            $counter += 1
+            if $counter > 20
+                $counter = 0
+            end
+            if $counter.between?(0,5) 
+                $shark_y += 1
+            end
+            if $counter.between?(5, 10)
+                $shark_x += 1
+            end
+            if $counter.between?(10, 15)
+                $shark_y -= 1
+            end
+            if $counter.between?(15, 20)
+                $shark_x -= 1
+            end
+    end
+    def movement(input)
+        if @row_position > 0 && $grid[@row_position - 1][@col_position] != $Island
+            if input == 'w' 
+                if @row_position - 1 == $shark_x && @col_position == $shark_y
+                    print "You hit the shark & you are dead!"
+                    input = "q"  
+                    
+                end
+                if $grid[@row_position - 1][@col_position] == $Waterfall
+                    message = "You found a waterfall!!!"  
+                    $grid[@row_position - 1][@col_position] = $Empty 
+                
+                else
+                    $message = "Water is all around!"     
+                end     
+                @row_position = @row_position - 1
+                      
+            end
         end
-        if $counter.between?(0,5) 
-            $shark_y += 1
+        if @col_position > 0 && $grid[@row_position][@col_position - 1] != $Island
+            if input == 'a'
+                if @row_position == $shark_x && @col_position - 1 == $shark_y
+                    print "You hit the shark & you are dead!"
+                    sleep(3)
+                    input = "q"        
+                end
+                if $grid[@row_position][@col_position - 1] == $Waterfall
+                    message = "You found a waterfall!!!" 
+                    #sleep(3) 
+                else
+                    message = "Water is all around!"     
+                end
+                @col_position = @col_position - 1
+                $grid[@row_position][@col_position] = $Empty
+            end
         end
-        if $counter.between?(5, 10)
-            $shark_x += 1
+        if @row_position < @gridSize - 1 && $grid[@row_position + 1][@col_position] != $Island
+            if input == 's'
+                if @row_position + 1 == $shark_x && @col_position == $shark_y
+                    print "You hit the shark & you are dead!"
+                    sleep(3)
+                    input = "q"               
+                end
+                if $grid[@row_position + 1][@col_position] == $Waterfall
+                    message = "You found a waterfall!!!"  
+                    #sleep(3) 
+                else
+                    message = "Water is all around!"     
+                end
+                @row_position = @row_position + 1
+                $grid[@row_position][@col_position] = $Empty
+            end
         end
-        if $counter.between?(10, 15)
-            $shark_y -= 1
+        if @col_position < @gridSize - 1 && $grid[@row_position][@col_position + 1] != $Island
+            if input == 'd'
+                if @row_position == $shark_x && @col_position + 1 == $shark_y
+                    print "You hit the shark & you are dead!"
+                    sleep(3)
+                    input = "q"          
+                end
+                if $grid[@row_position][@col_position + 1] == $Waterfall
+                    message = "You found a waterfall!!!"  
+                    #sleep(3)    
+                else
+                    $message = "Water is all around!" 
+                end
+                @col_position = @col_position + 1
+                $grid[@row_position][@col_position] = $Empty
+            end
         end
-        if $counter.between?(15, 20)
-            $shark_x -= 1
-        end
+        input
     end
 end
