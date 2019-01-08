9a99bce1eb1d17436ec117ce79863f4672da8223
Michael Huffman <huffman442@yahoo.com>
2019-01-08 14:31:56 -0500

    added health & shark damage system

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ const shark = 3
 const waterfall = 4
 let row_position = 0
 let col_position = 7
-const level1Arr = [[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 2, 2, 2, 0, 0],[0, 0, 0, 2, 0, 2, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0]]
+let shark_x = 5
+let shark_y = 3
+let counter = 0
+
+const level1Arr = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0],[0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0],[0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0],[0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 let currentlvlarray = level1Arr
-const health = 40
+let health = 40
+let maxHealth = 40
 
 let drawBoard = () => {
+    
     const container = document.getElementById('container');
     container.innerHTML = ""
     let i = 0
@@ -21,18 +27,18 @@ let drawBoard = () => {
                 const ele = document.createElement("div");
                 ele.setAttribute("id", `div${k}`);
                 if(col_position === j && row_position === i){
-                    ele.setAttribute("class", "boat")
+                    ele.setAttribute("class", "tile")
                     container.appendChild(ele);
-                    document.getElementById("div" + k).innerHTML = "<img src=img/ship_big_water_ripple.gif height = 50 width = 50>"
+                    document.getElementById("div" + k).innerHTML = "<img src=img/ship_big_water_ripple.gif height = 50px width= 50px>"
                 }  
                 else if(level1Arr[i][j] === water) {
-                    ele.setAttribute("class", "water")
+                    ele.setAttribute("class", "tile")
                     container.appendChild(ele);
-                    document.getElementById(`div${k}`).innerHTML = "<img src=img/water.jpg height = 50 width = 50>"
+                    document.getElementById(`div${k}`).innerHTML = "<img src=img/water.jpg height = 50px width = 50px>"
                 } else if(level1Arr[i][j] === island) {
-                    ele.setAttribute("class", "water")
+                    ele.setAttribute("class", "tile")
                     container.appendChild(ele);
-                    document.getElementById(`div${k}`).innerHTML = "<img src=img/GrassIsland.png height = 50 width = 50>"
+                    document.getElementById(`div${k}`).innerHTML = "<img src=img/GrassIsland.png height = 50px width = 50px>"
                 } 
             }
             j++
@@ -40,7 +46,13 @@ let drawBoard = () => {
         }
         i++
     }
-
+    if (!document.getElementById(`div${k}`)){
+        const ele = document.createElement("div");
+        ele.setAttribute("id", `div${k}`)
+        ele.setAttribute("class", "heathBar")
+        container.appendChild(ele)
+        document.getElementById(`div${k}`).innerHTML = `<img src=img/corazon.png height = 50px  width = 50px>${health}/${maxHealth}`
+    }
 }
 let moveLeft = () => {    
     if(col_position > 0 && currentlvlarray[row_position][col_position-1] != island) {
@@ -66,8 +78,31 @@ let moveUp = () => {
         drawBoard()
     }
 }
-let sharkMovement = () => {
-
+async function sharkMovement(){
+    
+    while(true){
+        setTimeout(() => {}, 4000)
+        console.log("hello")
+        if(counter > 20){
+            counter = 0
+        }
+        if(counter >= 0 && counter < 5){ 
+            shark_y++
+        }
+        if(counter >= 5 && counter < 10){
+            shark_x++
+        }
+        if(counter >= 10 && counter < 15){
+            shark_y--
+        }
+        if(counter >= 15 && counter < 20){
+            $shark_x--
+        }
+        drawBoard()
+    }
+}
+let sharkDamage = () => {
+    health -= Math.floor(Math.random()*4)
 }
 
 document.addEventListener("keypress", function onEvent(event) {
@@ -79,12 +114,11 @@ document.addEventListener("keypress", function onEvent(event) {
         moveDown()
     } else if (event.key === "d"){
         moveRight()
-    }
-    
+    }    
 });
 
 
 
-window.onload = drawBoard(level1Arr)
+window.onload = drawBoard() 
 
 
